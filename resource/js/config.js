1b0f5391d3ff3dd92f48cf285108f0c29b9e2363
f2er <f2erzheng@gmail.com>
2014-12-08 14:18:03 +0800

    update

--- resource/js/config.js
+++ resource/js/config.js
@@ -1,5 +1,5 @@
 seajs.config({
-    base: "./release/lib/",
+    /*base: "./release/lib/", */  
     alias: {
         "jquery": "jquery/jquery-1.10.2.min.js",
         "handlebars":"handlebars/handlebars-v1.1.0.js"
